84dcaea196d082d9572c7f7eea4a9a82773b9b84
Zeynep Gülsoy <196204993+zeyneppgulsoy@users.noreply.github.com>
2025-07-15 15:30:01 +0300

    Update App.tsx:Use taskData, and refactor task logic

---
+++ src/Data/index.ts
@@ -0,0 +1,33 @@
+export const taskData = [
+    {
+      title: 'Express Myself',
+      description: 'Set the building on fire.',
+      status: 'To Do',
+      assignee: 'Lyla Harper',
+    },
+    {
+      title: 'Catch Up Work - Saturday',
+      description: 'Gonna need you to come into work on Saturday.',
+      status: 'In Progress',
+      assignee: 'Hayes Aguirre',
+    },
+    {
+      title: 'Catch Up Work - Sunday',
+      description: 'Gonna need you to come into work on Sunday.',
+      status: 'In Progress',
+      assignee: 'Farah Koch',
+    },
+    {
+      title: 'Tips Reports',
+      description: 'Did you get the memo?',
+      status: 'To Do',
+      assignee: 'Salvador Vega',
+    },
+    {
+      title: 'Buy some more Flare',
+      description: 'Apparently, 13 is not the minimum number.',
+      status: 'Done',
+      assignee: 'Maria Koch',
+    },
+  ];
+  
---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+export interface TaskProps {
+    id: string; 
+    title: string;
+    description: string;
+    status: string;
+    assignee: string;
+}
